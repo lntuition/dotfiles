8f1b8f5ccdf4713dd49c5c5fc645c3dd1adbc03a
Sang-Heon Jeon <ekffu200098@gmail.com>
2021-04-05 21:43:10 +0900

    feat: Update mac.sh

--- mac.sh
+++ mac.sh
@@ -2,21 +2,28 @@
 
 # Package manager setting
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh
-which zsh
-chsh -s $(which zsh)
+brew install git
+
+# iTerm2 setting
+brew install --cask iterm2
+# Change default terminal to iterm
 
 # Oh-my-zsh setting
 brew install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Add git zsh-syntax-highlighting zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Add plugins ~/.zshrc git zsh-syntax-highlighting zsh-autosuggestions
+
+# Java setting
+brew tap AdoptOpenJDK/openjdk
+brew install --cask 11
 
 # Rust setting
 curl -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 # Git setting
-brew install git
 git config --global user.email 'ekffu200098@gmail.com'
 git config --global user.name 'Sang-Heon Jeon'
 
